090860853b3384a8f1b4d8dbb25cde1ef17fb61f
Bálint Fodor <balint.fodor@gmail.com>
2014-09-03 19:18:19 +0200

    closes #6, added comments, and argument warning

--- clustermerger/cluster_merger.py
+++ clustermerger/cluster_merger.py
@@ -5,15 +5,27 @@ from scipy.cluster.vq import kmeans2, whiten
 from sklearn.cluster import KMeans
 from sklearn import metrics
 
+if len(sys.argv) < 3:
+    print("Usage: %s <first csv> <second csv>" % sys.argv[0])
+    sys.exit(-1)
+
 csv1 = sys.argv[1]
 csv2 = sys.argv[2]
 
+# location id and image id are the index together
 f1 = pd.io.parsers.read_csv(csv1, header=None, index_col=[0,1], engine='c', delimiter=";")
 f2 = pd.io.parsers.read_csv(csv2, header=None, index_col=[0,1], engine='c', delimiter=";")
 
+# merge the tables by joining indices
+#
+# file1: loc_id, img_id, clust_id
+# file2: loc_id, img_id, clust_id
+# merged: loc_id, img_id, clust_id_file1, clust_id_file2
 merged = f1.merge(f2, left_index=True, right_index=True)
 clust_id_dict = dict()
 clust_num_dict = dict()
+# the new cluster id is the fields clust_id_file1 and clust_id_file2 together
+# the new cluster ids are converted into numbers
 for ix, row in merged.iterrows():
     new_clust = row[0] + "_" + row[1]
     key = (ix[0], new_clust)
