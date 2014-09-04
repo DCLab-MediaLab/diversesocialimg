10c4c45cea281ab40eec949bf6bfdc7d7590063d
Bálint Fodor <balint.fodor@gmail.com>
2014-09-04 09:06:04 +0200

    #5 select the best item from the dataset

--- gaboralgorithm/gabor_algorithm.py
+++ gaboralgorithm/gabor_algorithm.py
@@ -6,9 +6,6 @@ if len(sys.argv) < 3:
     print("Usage: %s <clusters csv> <relevance csv>" % sys.argv[0])
     sys.exit(-1)
 
-def p_at_n(order, n):
-
-
 cluster_csv = sys.argv[1]
 relevance_csv = sys.argv[2]
 
@@ -17,4 +14,19 @@ rel = pd.io.parsers.read_csv(relevance_csv, header=None, engine='c', delimiter="
 
 merged = clust.merge(rel, left_index=True, right_index=True)
 
+loc_ids = pd.Series(merged['0_x'].values.ravel()).unique()
 
+for loc_id in loc_ids:
+    data = merged[merged['0_x'] == loc_id]
+    order = []
+    # select the best item
+    maxidx = data['0_y'].idxmax()
+    order += [data.ix[maxidx][1]]
+    # remove from the list
+    data = data.drop(maxidx)
+
+    # TODO: select the next best
+    # for 
+
+    print(order)
+    
